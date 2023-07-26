d69f97ae83d2ec8931093cc242bfe04575254187
Alan Liang <aiu.lun@gmail.com>
2023-07-26 14:45:06 -0700

    feat: refactored to express and added styling

--- server.js
+++ server.js
@@ -1,63 +1,42 @@
-const http = require('http');
-const fs = require('fs')
-const url = require('url');
-const querystring = require('querystring');
+const express = require('express')
+const cors = require('cors')
+const app = express()
 const figlet = require('figlet')
 
-const server = http.createServer((req, res) => {
-  const page = url.parse(req.url).pathname;
-  const params = querystring.parse(url.parse(req.url).query);
-  console.log(page);
+const PORT = 8001;
 
-  const readWrite = (file, contentType) => {
-    fs.readFile(file, function(err, data) {
-      res.writeHead(200, { 'Content-Type' : contentType });
-      res.write(data);
-      res.end();
-    })
-  }
+app.use(express.static(__dirname + '/public'));
 
-  switch (page) {
-    case '/':
-      readWrite('index.html', 'text/html');
-      break;
-    case '/otherpage':
-      readWrite('otherpage.html', 'text/html');
-      break;
-    case '/otherotherpage':
-      readWrite('otherotherpage.html', 'text/html');
-      break;
-    case '/api':
-      let flipRes = `Type 'flip' in the input box`
-      if (params['student'] == 'flip') {
-        flipRes = Math.ceil(Math.random() * 2) === 1 ? 'heads' : 'tails'
-      }
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      const objToJson = {
-        flip: flipRes
-      }
-      res.end(JSON.stringify(objToJson));
-      break;
-    case '/css/style.css':
-      fs.readFile('css/style.css', function(err, data) {
-        res.write(data);
-        res.end();
-      });
-      break;
-    case '/js/main.js':
-      readWrite('js/main.js', 'text/javascript');
-      break;
-    default:
-      figlet('404!!', function(err, data) {
-        if (err) {
-            console.log('Something went wrong...');
-            console.dir(err);
-            return;
-        }
-        res.write(data);
-        res.end();
-      });
-  }
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html');
 });
 
-server.listen(8000);
+app.get('/api', (request, response) => {
+  const params = request.query; // Parse query parameters from the request URL
+
+  let flipRes = `Use the 'Flip' button`;
+  if (params['student'] === 'flip') {
+    flipRes = Math.random() < 0.5 ? 'heads' : 'tails';
+  }
+
+  const objToJson = {
+    flip: flipRes
+  };
+  response.json(objToJson); // Use response.json() to send a JSON response
+});
+
+app.use((request, response) => {
+  figlet('404!!', function(err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      response.status(500).send('Internal Server Error'); // Send an error response
+    } else {
+      response.status(404).send(data); // Send the 'data' as the 404 response
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
